2cf2f052aa017a4a88b17cc33165b6f2ec6b7a8d
danpatoneil <danpatoneil@gmail.com>
2024-02-24 20:02:33 -0500

    Adding comments to code, and updating README

--- README.md
+++ README.md
@@ -7,11 +7,33 @@ this website is a password generator. It uses Javascript to prompt the user for 
 This application runs in your browser and does not require installation.
 
 Here is a screenshot and a link to the running website
-[Password Generator](https://duckduckgo.com)
+[Password Generator](https://danpatoneil.github.io/Password-Generator/)
 ![a screenshot of the website. It shows the password generator page, which has a text box for the password and a button to generate the password.](screenshot.png)
 
 ## Use
-This project will allow users to generate secure passwords from 
+This project will allow users to generate secure passwords by selecting an entered number of characters from several lists of characters that the user agrees to.
 
 ## Future iterations
-In future iterations of this project, I intend to 
+The major flaw of this application is that it uses Javascript only to get user input. These options would be best addressed through a form like a list of check boxes or text entry that only allows numbers, rather than being forced to check that the input is a number using Javascript. The prompt interface is the best purely Javascript tool for this job but it would be highly preferrable if we could use HTML and CSS to change the password generator screen to have a regular form or even a custom text entry for which special characters the user wants in their password.
+
+MIT License
+
+Copyright (c) 2024 Daniel O'Niel
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- script.js
+++ script.js
@@ -11,9 +11,10 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
+// Get information from the user and generate a secure password
 function generatePassword(){
   var length = 0;
+  // Loops until the user has entered a valid number of characters for the password. Checks if the entered unit is a number and if it is within the valid range. If not, tells the user to enter a valid number and loops.
   do{
     var password = "";
     var input = prompt("How long would you like your password to be? Please enter a number between 8 and 128");
@@ -23,7 +24,9 @@ function generatePassword(){
       length = input;
     }
   }while(length === 0);
+  // This variable holds the sum of all of the accepted character sets the user agrees to.
   var charSet = "";
+  // Loops infinitely until the user says yes to at least one of the proposed sets of characters
   do{
     charSet = checkCharSet();
   }while(charSet === "");
@@ -33,10 +36,10 @@ function generatePassword(){
   }
   return password;
 }
-
+// A series of confirm prompts to ask the user if they would like to opt into each individual character set. For clarity's sake I would have preferred to do this as check boxes on a form, but there is no built in functionality for prompts with multiple discreet options. I considered writing it such that the user entered a number 1-4 for each character set they wanted to opt into. That would have required fewer popup prompts but it would also have been very clunky in my opintion both to use and to code.
 function checkCharSet(){
+  // Asks the user whether or not they'd like to opt into any individual set of characters. If they say no to all of them it simply returns a blank string, but not before informing the user they'll have to choose at least one set of characters to generate a password from
   var output = "";
-  
   if(confirm("Would you like your password to include uppper case letters?")){
     output+="ABDCEFGHIJKLMNOPQRSTUVWXYZ";
   }
@@ -47,8 +50,9 @@ function checkCharSet(){
     output+="0123456798";
   }
   if(confirm("Would you like your password to include special characters?")){
-    output+="!#$%&()*+=-_.,;:<>@?[]\\^`{}|~";
+    output+="'!#$%&()*+=-_.,;:<>@?[]\\^`{}|~\"";
   }
+  output+=prompt("Would you like any other characters allowed in your password");
   if(output === ""){
     alert("Please choose at least one set of characters to include in your password.")
   }
